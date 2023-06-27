3a90e1ae25290f6b8ad9fbdcfa12ffb22db4a867
Leslie <91215520+siRenisCat@users.noreply.github.com>
2023-06-26 21:25:43 -0400

    Update 23_Merge_k_Sorted_Lists.py
    brute force added

--- 23_Merge_k_Sorted_Lists.py
+++ 23_Merge_k_Sorted_Lists.py
@@ -1,15 +1,35 @@
-You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
-Merge all the linked-lists into one sorted linked-list and return it.
+# You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
+# Merge all the linked-lists into one sorted linked-list and return it.
 
-Example 1:
+# Example 1:
 
-Input: lists = [[1,4,5],[1,3,4],[2,6]]
-Output: [1,1,2,3,4,4,5,6]
-Explanation: The linked-lists are:
-[
-  1->4->5,
-  1->3->4,
-  2->6
-]
-merging them into one sorted list:
-1->1->2->3->4->4->5->6
+# Input: lists = [[1,4,5],[1,3,4],[2,6]]
+# Output: [1,1,2,3,4,4,5,6]
+# Explanation: The linked-lists are:
+# [
+#   1->4->5,
+#   1->3->4,
+#   2->6
+# ]
+# merging them into one sorted list:
+# 1->1->2->3->4->4->5->6
+
+class LinkedNode(self, val):
+  self.val = val
+  self.next = next
+
+class Solution:
+  def mergeKLists(self, lists):
+    #brute force
+    node = []
+    head = point = ListNode(0)
+
+    for l in lists:
+      while l:
+        node.append(l.val)
+
+    while j in sorted(node):
+      point.next = ListNode(j)
+      point = point.next
+
+    return head.next
