7093d3a8a93272965bb86c382ffd33a3fea4af04
Leslie <91215520+siRenisCat@users.noreply.github.com>
2023-06-30 10:58:58 -0400

    Update 234. Palindrome Linked List.py

--- 234. Palindrome Linked List.py
+++ 234. Palindrome Linked List.py
@@ -8,7 +8,34 @@ class LinkedList
 
 class Solution
     def __init__(self, head):
-        # 
+        # empty list
         if head is None:
-              return
-  
+              return False
+
+        slow = head
+        fast = head
+
+        # iterate to half of the linked list
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        # reversed the second half of the linked list
+        cur = slow
+        prev = None
+        while cur:
+            tmp = cur.next
+            cur.next = prev
+            prev = cur
+            cur = tmp
+        # compare the reversed second half with the first half
+        ptr = head
+        while prev:
+            if prev.val == ptr.val:
+                prev = prev.next
+                ptr = ptr.next
+            else:
+                return False
+        return True
+        
+            
