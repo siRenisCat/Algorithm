a539f902bda3ab33cf815b465b21506b9195eb66
Leslie <91215520+siRenisCat@users.noreply.github.com>
2023-06-30 10:35:13 -0400

    Create 234. Palindrome Linked List.py

---
+++ 234. Palindrome Linked List.py
@@ -0,0 +1,14 @@
+# Given the head of a singly linked list, return true if it is a palindrome or false otherwise.
+
+
+class LinkedList
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution
+    def __init__(self, head):
+        # 
+        if head is None:
+              return
+  
